87a503929bef7f288fa3f54bfe65fb9e8ebb5045
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2023-12-11 15:53:02 +0800

    set different overlap for top and bottom

--- image_blending_reverse.py
+++ image_blending_reverse.py
@@ -4,32 +4,47 @@ import cv2
 import numpy as np
 
 
+extend_top = 1
+extend_bottom = 10
+
+# class Capture:
+#     ScansPerFrame=188
+#     Blocks = 4
+#     BlockArray_frames = [32, 94, 156, 188]
+#     BlockArray_crops_top_pixels = [16, 40, 45, 20]
+#     BlockArray_crops_bottom_pixels = [40, 40, 35, 36]
+#     BlockArray_overlap_top_pixels = [x-1 for x in BlockArray_crops_top_pixels]
+#     BlockArray_overlap_bottom_pixels = [x-1 for x in BlockArray_crops_bottom_pixels]
+#     class Strip:
+#         Width=4608
+#         Height=96
+
 class Capture:
-    ScansPerFrame=188
+    ScansPerFrame=208
     Blocks = 4
-    BlockArray_frames = [32, 94, 156, 188]
-    BlockArray_crops_top_pixels = [16, 40, 45, 20]
-    BlockArray_crops_bottom_pixels = [40, 40, 35, 36]
-    # BlockArray_crops_top_pixels = [0, 0, 0, 0]
-    # BlockArray_crops_bottom_pixels = [0, 0, 0, 0]
+    BlockArray_frames = [40, 104, 168, 208]
+    BlockArray_crops_top_pixels = [6, 20, 25, 20]
+    BlockArray_crops_bottom_pixels = [56, 60, 55, 42]
+    BlockArray_overlap_top_pixels = [x-extend_top for x in BlockArray_crops_top_pixels]
+    BlockArray_overlap_bottom_pixels = [x-extend_bottom for x in BlockArray_crops_bottom_pixels]
     class Strip:
         Width=4608
         Height=96
 
-def calWeight(d, k):
+def calWeight(overlap_top, overlap_bottom, k):
     '''
     :param d: 融合重叠部分直径
     :param k: 融合计算权重参数
     :return:
     '''
 
-    x = np.arange(-d / 2, d / 2)
+    x = np.arange(-overlap_top, overlap_bottom)
     # y = 1 / (1 + np.exp(-k * x)) #exponenital
-    y = x/d + 0.5 # linear
+    y = x/(overlap_top+overlap_bottom) + overlap_top/(overlap_top+overlap_bottom) # linear
     return y
 
 
-def imgFusion(img1, img2, overlap, left_right=True):
+def imgFusion(img1, img2, overlap_top, overlap_bottom, left_right=True):
     '''
     图像加权融合
     :param img1:
@@ -39,30 +54,36 @@ def imgFusion(img1, img2, overlap, left_right=True):
     :return:
     '''
     # 这里先暂时考虑平行向融合
-    w = calWeight(overlap, 0.05)  # k=5 这里是超参
+    w = calWeight(overlap_top, overlap_bottom, 0.05)  # k=5 这里是超参
 
-    if left_right:  # 左右融合
-        col, row = img2.shape
-        img_new = np.zeros((row, 2 * col - overlap))
-        img_new[:, :col] = img1
-        w_expand = np.tile(w, (col, 1))  # 权重扩增
-        img_new[:, col - overlap:col] = (1 - w_expand) * img1[:, col - overlap:col] + w_expand * img2[:, :overlap]
-        img_new[:, col:] = img2[:, overlap:]
-    else:  # 上下融合
-        row1, col = img1.shape
-        row2, col = img2.shape
+    # if left_right:  # 左右融合
+    #     col, row = img2.shape
+    #     img_new = np.zeros((row, 2 * col - overlap))
+    #     img_new[:, :col] = img1
+    #     w_expand = np.tile(w, (col, 1))  # 权重扩增
+    #     img_new[:, col - overlap:col] = (1 - w_expand) * img1[:, col - overlap:col] + w_expand * img2[:, :overlap]
+    #     img_new[:, col:] = img2[:, overlap:]
+    # else:
 
-        img_new = np.zeros((row1+row2 - overlap, col))
-        img_new[:row1, :] = img1
-        w = np.reshape(w, (overlap, 1))
-        w_expand = np.tile(w, (1, col))
-        img_new[row1 - overlap:row1, :] = (1 - w_expand) * img1[row1 - overlap:row1, :] + w_expand * img2[:overlap, :]
-        img_new[row1:, :] = img2[overlap:, :]
+    # 上下融合
+    row1, col = img1.shape
+    row2, col = img2.shape
+    print('img1.shape, img2.shape')
+    print(img1.shape, img2.shape)
+
+    img_new = np.zeros((row1+row2 - overlap_top - overlap_bottom, col))
+    print('img_new[:row1, :].shape, img1.shape')
+    print(img_new[:row1, :].shape,img1.shape )
+    img_new[:row1, :] = img1
+    w = np.reshape(w, (overlap_top+overlap_bottom, 1))
+    w_expand = np.tile(w, (1, col))
+    img_new[row1 - overlap_top - overlap_bottom:row1, :] = (1 - w_expand) * img1[row1 - overlap_top - overlap_bottom:row1, :] + w_expand * img2[:(overlap_top+overlap_bottom), :]
+    img_new[row1:, :] = img2[(overlap_top+overlap_bottom):, :]
 
     return img_new
 
 
-def channelFusion(image_list, length, overlap):
+def channelFusion(image_list, length):
     for i in range(length):
         print(i)
         if i < Capture.BlockArray_frames[0]:
@@ -70,38 +91,52 @@ def channelFusion(image_list, length, overlap):
             up_crop_bottom = Capture.BlockArray_crops_bottom_pixels[0]
             down_crop_top = Capture.BlockArray_crops_top_pixels[0]
             down_crop_bottom = Capture.BlockArray_crops_bottom_pixels[0]
+            overlap_top = Capture.BlockArray_overlap_top_pixels[0]
+            overlap_bottom = Capture.BlockArray_overlap_bottom_pixels[0]
         elif i == Capture.BlockArray_frames[0]:
             up_crop_top = Capture.BlockArray_crops_top_pixels[0]
             up_crop_bottom = Capture.BlockArray_crops_bottom_pixels[0]
             down_crop_top = Capture.BlockArray_crops_top_pixels[1]
             down_crop_bottom = Capture.BlockArray_crops_bottom_pixels[1]
+            overlap_top = Capture.BlockArray_overlap_top_pixels[1]
+            overlap_bottom = Capture.BlockArray_overlap_bottom_pixels[0]
         elif i < Capture.BlockArray_frames[1]:
             up_crop_top = Capture.BlockArray_crops_top_pixels[1]
             up_crop_bottom = Capture.BlockArray_crops_bottom_pixels[1]
             down_crop_top = Capture.BlockArray_crops_top_pixels[1]
             down_crop_bottom = Capture.BlockArray_crops_bottom_pixels[1]
+            overlap_top = Capture.BlockArray_overlap_top_pixels[1]
+            overlap_bottom = Capture.BlockArray_overlap_bottom_pixels[1]
         elif i == Capture.BlockArray_frames[1]:
             up_crop_top = Capture.BlockArray_crops_top_pixels[1]
             up_crop_bottom = Capture.BlockArray_crops_bottom_pixels[1]
             down_crop_top = Capture.BlockArray_crops_top_pixels[2]
             down_crop_bottom = Capture.BlockArray_crops_bottom_pixels[2]
+            overlap_top = Capture.BlockArray_overlap_top_pixels[2]
+            overlap_bottom = Capture.BlockArray_overlap_bottom_pixels[1]
         elif i < Capture.BlockArray_frames[2]:
             up_crop_top = Capture.BlockArray_crops_top_pixels[2]
             up_crop_bottom = Capture.BlockArray_crops_bottom_pixels[2]
             down_crop_top = Capture.BlockArray_crops_top_pixels[2]
             down_crop_bottom = Capture.BlockArray_crops_bottom_pixels[2]
+            overlap_top = Capture.BlockArray_overlap_top_pixels[2]
+            overlap_bottom = Capture.BlockArray_overlap_bottom_pixels[2]
         elif i == Capture.BlockArray_frames[2]:
             up_crop_top = Capture.BlockArray_crops_top_pixels[2]
             up_crop_bottom = Capture.BlockArray_crops_bottom_pixels[2]
             down_crop_top = Capture.BlockArray_crops_top_pixels[3]
             down_crop_bottom = Capture.BlockArray_crops_bottom_pixels[3]
+            overlap_top = Capture.BlockArray_overlap_top_pixels[3]
+            overlap_bottom = Capture.BlockArray_overlap_bottom_pixels[2]
         else:
             up_crop_top = Capture.BlockArray_crops_top_pixels[3]
             up_crop_bottom = Capture.BlockArray_crops_bottom_pixels[3]
             down_crop_top = Capture.BlockArray_crops_top_pixels[3]
             down_crop_bottom = Capture.BlockArray_crops_bottom_pixels[3]
+            overlap_top = Capture.BlockArray_overlap_top_pixels[3]
+            overlap_bottom = Capture.BlockArray_overlap_bottom_pixels[3]
 
-        if overlap == 0:
+        if (overlap_top+overlap_bottom) == 0:
             if i == 0:
                 # Read the BMP strip
                 img_up = cv2.rotate(cv2.imread(image_list[i], cv2.IMREAD_GRAYSCALE), cv2.ROTATE_180)
@@ -129,25 +164,34 @@ def channelFusion(image_list, length, overlap):
 
                 img_up = img_up[up_crop_top:, :]
             elif i == length-1:
-                img_up = img_up[:-(up_crop_bottom - int(overlap / 2)), :]
-
+                img_up = img_up[:-(up_crop_bottom - overlap_bottom), :]
+                print(-(up_crop_bottom - overlap_bottom))
                 img_down = cv2.rotate(cv2.imread(image_list[i], cv2.IMREAD_GRAYSCALE), cv2.ROTATE_180)
-                img_down = img_down[down_crop_top - int(overlap / 2):-down_crop_bottom, :]
-
+                # to fix, for the last strip
+                # if i == len(image_list)-1:
+                #     img_down = img_down[down_crop_top - overlap_top:-down_crop_bottom, :]
+                # else:
+                img_down = img_down[down_crop_top - overlap_top:, :]
+                print(down_crop_top - overlap_top, -down_crop_bottom)
                 img_up = (img_up - img_up.min()) / img_up.ptp()
                 img_down = (img_down - img_down.min()) / img_down.ptp()
-                img_up = imgFusion(img_up, img_down, overlap=overlap, left_right=False)
+
+                print('img_up.shape, img_down.shape')
+                print(img_up.shape, img_down.shape)
+                img_up = imgFusion(img_up, img_down, overlap_top=overlap_top, overlap_bottom=overlap_bottom, left_right=False)
                 img_up = np.uint16(img_up * 65535)
 
             else:
-                img_up = img_up[:-(up_crop_bottom - int(overlap / 2)), :]
+                img_up = img_up[:-(up_crop_bottom - overlap_bottom), :]
 
                 img_down = cv2.rotate(cv2.imread(image_list[i], cv2.IMREAD_GRAYSCALE), cv2.ROTATE_180)
-                img_down = img_down[down_crop_top - int(overlap / 2):, :]
+                img_down = img_down[down_crop_top - overlap_top:, :]
 
                 img_up = (img_up - img_up.min()) / img_up.ptp()
                 img_down = (img_down - img_down.min()) / img_down.ptp()
-                img_up = imgFusion(img_up, img_down, overlap=overlap, left_right=False)
+                print('img_up.shape, img_down.shape')
+                print(img_up.shape, img_down.shape)
+                img_up = imgFusion(img_up, img_down, overlap_top=overlap_top, overlap_bottom=overlap_bottom, left_right=False)
                 img_up = np.uint16(img_up * 65535)
 
     return img_up
@@ -155,8 +199,7 @@ def channelFusion(image_list, length, overlap):
 path = r'..\Dataset\strip'
 folder_list = glob.glob(f'{path}/*')
 
-overlap = 20
-for folder in folder_list[:1]:
+for folder in folder_list[2:3]:
     print(folder)
     image_list = glob.glob(f'{folder}/*.bmp')
     ScansPerFrame = int(len(image_list)/3)
@@ -165,19 +208,20 @@ for folder in folder_list[:1]:
     G_list = image_list[ScansPerFrame:ScansPerFrame*2]
     B_list = image_list[2*ScansPerFrame:ScansPerFrame*3]
 
-    # length = len(R_list)
-    length = 94
+    length = len(R_list)
+    # length = 32
 
-    R_fusion = channelFusion(R_list, length, overlap)
-    G_fusion = channelFusion(G_list, length, overlap)
-    B_fusion = channelFusion(B_list, length, overlap)
+    R_fusion = channelFusion(R_list, length)
+    G_fusion = channelFusion(G_list, length)
+    B_fusion = channelFusion(B_list, length)
     # print(R_fusion.shape, G_fusion.shape)
     # cv2.merge 实现图像通道的合并
     imgMerge = cv2.merge([B_fusion, G_fusion, R_fusion])
 
     # print(R_fusion.shape,G_fusion.shape,B_fusion.shape)
 
-cv2.imwrite(f'./blend-overlap-{overlap}.png', imgMerge)
+# cv2.imwrite(f'./blend-overlap-{overlap}.png', imgMerge)
+cv2.imwrite(f'./blend-extend-top-{extend_top}-bottom-{extend_bottom}.png', imgMerge)
 # cv2.imshow('Stitched Image', img_new)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
