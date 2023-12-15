6e5820d10ba104f6087d8937f9ccf283fe13e5dc
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2023-12-15 17:01:45 +0800

    histogram matching module

---
+++ hist_matching.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+
+def hist_specification(image, reference):
+    hist_image = cv2.calcHist([image], [0], None, [256], [0,256])
+    hist_reference = cv2.calcHist([reference], [0], None, [256], [0,256])
+
+    cdf_image = hist_image.cumsum()/hist_image.sum()
+    cdf_reference = hist_reference.cumsum()/hist_reference.sum()
+
+    lut = np.interp(cdf_image, cdf_reference, range(256))
+    matched_image = cv2.LUT(image, lut.astype('uint8'))
+
+    return matched_image
+
+ref = cv2.cvtColor(cv2.imread('2.bmp', cv2.IMREAD_COLOR), cv2.COLOR_BGR2HSV)
+img = cv2.cvtColor(cv2.imread('1.png', cv2.IMREAD_COLOR), cv2.COLOR_BGR2HSV)
+
+ref_h, ref_s, ref_v =cv2.split(ref)
+img_h, img_s, img_v =cv2.split(img)
+
+matched_h = hist_specification(img_h, ref_h)
+
+matched_img = cv2.cvtColor(cv2.merge([matched_h, img_s, img_v]),cv2.COLOR_HSV2BGR)
+
+cv2.imwrite(f'./3.png', matched_img)
