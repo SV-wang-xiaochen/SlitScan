9d8ca4c5affd4b834d9396005ed7e69cfc50a63a
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2024-01-09 11:27:13 +0800

    crop image to view result comparison

--- detail_view.py
+++ detail_view.py
@@ -5,8 +5,22 @@ import numpy as np
 from hist_matching import hist_matching
 import time
 
-i1= cv2.imread('i1.png')
-i2= cv2.imread('i2.png')
+orig = cv2.imread('blend.png')
+blur1 = cv2.imread('blend_blur_9.png')
+blur2 = cv2.imread('blend_blur_15.png')
 
-cv2.imwrite('i1-1.png', i1[0:1000,1800:2800])
-cv2.imwrite('i2-1.png', i2[0:1000,1800:2800])
+orig_crop = orig[0:1000,1800:2800]
+blur1_crop = blur1[0:1000,1800:2800]
+blur2_crop = blur2[0:1000,1800:2800]
+
+img_save = cv2.hconcat(([orig_crop, blur1_crop, blur2_crop]))
+
+cv2.imwrite('r1.png', img_save)
+
+orig_crop = orig[1800:2800,1800:2800]
+blur1_crop = blur1[1800:2800,1800:2800]
+blur2_crop = blur2[1800:2800,1800:2800]
+
+img_save = cv2.hconcat(([orig_crop, blur1_crop, blur2_crop]))
+
+cv2.imwrite('r2.png', img_save)
