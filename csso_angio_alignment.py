5f68a5f210ae3175adbe426d32ef7efccd923525
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2024-03-06 18:07:23 +0800

    csso angio aligment on a batch of files

--- csso_angio_alignment.py
+++ csso_angio_alignment.py
@@ -1,31 +1,66 @@
 import cv2
 import numpy as np
+import glob
+import os
+import json
 
-# Load the two images
-src = cv2.imread(r'D:\Projects\Dataset\AngioAnnotation\labeled\00101_20240229008_00101_20220523001_OD_2022-05-23_10-32-15Angio 12x12 1024x1024 R2_27.6244_cSSO_2024-02-29_14-35-41.png')
-dstRef = cv2.imread(r'D:\Projects\Dataset\AngioAnnotation\labeled\00101_20240229008_00101_20220523001_OD_2022-05-23_10-32-15Angio 12x12 1024x1024 R2_27.6244_EnhancedAngio_Retina.png')
+data_path = r"D:\Projects\Dataset\AngioAnnotation\labeled"
+result_path = f'{data_path}/results'
+os.makedirs(result_path, exist_ok=True)
 
-srcTri = np.array([[195, 913], [985, 251], [147, 28]]).astype(np.float32)
-dstTri = np.array([[197, 928], [967, 261], [139, 43]]).astype(np.float32)
+img_list_all = glob.glob(f'{data_path}/*.png')
+img_list_csso = []
+img_list_angio = []
 
-warp_mat = cv2.getAffineTransform(srcTri, dstTri)
+for i, img_path in enumerate(img_list_all):
+    if i%2 == 0:
+        img_list_csso.append(img_path)
+    else:
+        img_list_angio.append(img_path)
 
-dstWarp = cv2.warpAffine(src, warp_mat, (dstRef.shape[1], dstRef.shape[0]))
+json_list_all = glob.glob(f'{data_path}/*.json')
+json_list_csso = []
+json_list_angio = []
 
-# # Ensure both images have the same dimensions
-# image1 = cv2.resize(image1, (image2.shape[1], image2.shape[0]))
-#
-# Create an output image with the same size as the input images
-output = np.zeros_like(dstRef)
+for i, json_path in enumerate(json_list_all):
+    if i%2 == 0:
+        json_list_csso.append(json_path)
+    else:
+        json_list_angio.append(json_path)
 
-# Assign different colors for each image
-# output[:, :, 0] = dstWarp[:, :, 0]
-output[:, :, 1] = dstWarp[:, :, 1]
-output[:, :, 2] = dstRef[:, :, 2]
+for i in range(len(img_list_csso)):
+    json_path_csso = json_list_csso[i]
+    json_path_angio = json_list_angio[i]
 
-# # Display the result
-# cv2.imshow('Overlapped Images', result)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    with open(json_path_csso) as json_file:
+        data = json.load(json_file)
 
-cv2.imwrite('alignment.jpg', output)
+    srcTri = []
+    for shape in data['shapes']:
+        srcTri.append(shape['points'][0])
+    srcTri = np.array(srcTri).astype(np.float32)
+
+    with open(json_path_angio) as json_file:
+        data = json.load(json_file)
+    dstTri = []
+    for shape in data['shapes']:
+        dstTri.append(shape['points'][0])
+    dstTri = np.array(dstTri).astype(np.float32)
+
+    src = cv2.imread(img_list_csso[i])
+    dstRef = cv2.imread(img_list_angio[i])
+
+    warp_mat = cv2.getAffineTransform(srcTri, dstTri)
+
+    dstWarp = cv2.warpAffine(src, warp_mat, (dstRef.shape[1], dstRef.shape[0]))
+
+    # Create an output image with the same size as the input images
+    output = np.zeros_like(dstRef)
+
+    # Assign different colors for each image
+    # output[:, :, 0] = dstWarp[:, :, 0]
+    output[:, :, 1] = dstWarp[:, :, 1]
+    output[:, :, 2] = dstRef[:, :, 2]
+
+    base_name = os.path.basename(img_list_csso[i])
+    cv2.imwrite(f'{result_path}/{base_name}', output)
