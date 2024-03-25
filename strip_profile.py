fe81b3175e559d40c5bfd00f9804191a1758250a
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2024-03-25 16:59:21 +0800

    calculate and plot height and average intensity of bright region

--- strip_profile.py
+++ strip_profile.py
@@ -1,8 +1,10 @@
 import os.path
-
+import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import glob
+
+crop = 50
 
 data_path = "./strips"
 result_path = "./strips/results"
@@ -18,16 +20,49 @@ for img_path in img_list:
     # half_max_values = np.tile(half_max_values, (row, 1))
 
     above_half_max = np.where(strip>50, 1, 0)
-    print(above_half_max.shape)
-    print(above_half_max)
+
     # _, binary = cv2.threshold(above_half_max,1,254, cv2.THRESH_BINARY)
 
     # Create a spherical structuring element with a radius of 5
     se = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 
     # Perform morphological closing
-    close_image = cv2.morphologyEx(np.uint8(above_half_max), cv2.MORPH_CLOSE, se)
+    mask = cv2.morphologyEx(np.uint8(above_half_max), cv2.MORPH_CLOSE, se)
+
+    mask[:crop, :] = 0
+    mask[-crop:,:] = 0
 
     base_name = os.path.basename(img_path).split('.')[0]
     # # print(f'{result_path}/{base_name}-above-half.png')
-    cv2.imwrite(f'{result_path}/{base_name}-above-50-close.png', close_image*255)
+    cv2.imwrite(f'{result_path}/{base_name}-above-50-close-crop.png', mask*255)
+
+    increment = np.arange(row)
+    increment = np.reshape(increment, (row,1))
+    increment = np.tile(increment, (1, col))
+    increment[:crop, :] = 0
+    increment[-crop:, :] = 0
+
+    increment_masked = np.multiply(increment, mask)
+    mid_curve = np.sum(increment_masked, 0)/np.sum(mask, 0)
+
+    # height of bright region
+    bright_region_height = np.sum(mask, 0)
+
+    # average intensity of bright region
+    aver_intensity = np.divide(np.sum(np.multiply(strip, mask), 0), np.sum(mask, 0)+0.001)
+
+    # cv2.imwrite(f'{result_path}/{base_name}-1.png', increment_masked)
+    #
+    #
+    # # a = np.sum(np.multiply(increment, mask), 0)
+    # # b = np.sum(mask, 0)
+    # # mid_curve = np.divide(np.sum(np.multiply(increment, mask), 0), np.sum(mask, 0)+1)
+    # # # print(a)
+    # # # #
+    # Plotting the curve
+    plt.plot(aver_intensity)
+    ax = plt.gca()  # you first need to get the axis handle
+    ax.set_aspect(3)  # sets the height to width ratio to 1.5.
+    # plt.ylim(100, 150)
+    plt.tight_layout()
+    plt.show()
