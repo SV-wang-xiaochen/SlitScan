83371965a26ae151b2532d35c1bccc4a8a07113e
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2024-03-25 15:05:22 +0800

    morphological close

--- strip_profile.py
+++ strip_profile.py
@@ -13,11 +13,11 @@ img_list = glob.glob(f'{data_path}/*.bmp')
 for img_path in img_list:
     strip = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
     row, col = strip.shape
-    half_max_values = np.max(strip, axis=0) / 2
-    half_max_values = np.reshape(half_max_values, (1, 4608))
-    half_max_values = np.tile(half_max_values, (row, 1))
+    # half_max_values = np.max(strip, axis=0) / 2
+    # half_max_values = np.reshape(half_max_values, (1, 4608))
+    # half_max_values = np.tile(half_max_values, (row, 1))
 
-    above_half_max = np.where(strip>half_max_values, 1, 0)
+    above_half_max = np.where(strip>50, 1, 0)
     print(above_half_max.shape)
     print(above_half_max)
     # _, binary = cv2.threshold(above_half_max,1,254, cv2.THRESH_BINARY)
@@ -26,8 +26,8 @@ for img_path in img_list:
     se = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 
     # Perform morphological closing
-    open_image = cv2.morphologyEx(np.uint8(above_half_max), cv2.MORPH_OPEN, se)
+    close_image = cv2.morphologyEx(np.uint8(above_half_max), cv2.MORPH_CLOSE, se)
 
     base_name = os.path.basename(img_path).split('.')[0]
     # # print(f'{result_path}/{base_name}-above-half.png')
-    cv2.imwrite(f'{result_path}/{base_name}-above-half-open.png', open_image*255)
+    cv2.imwrite(f'{result_path}/{base_name}-above-50-close.png', close_image*255)
